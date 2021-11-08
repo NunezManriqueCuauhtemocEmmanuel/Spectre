b67ded3602b58cfe35abee7e4c0b34a01852fcb9
Nuñez Manrique Cuauhtémoc Emmanuel <76032890+NunezManriqueCuauhtemocEmmanuel@users.noreply.github.com>
2021-11-07 23:00:58 -0600

    inicio 4

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 let menu = document.querySelector('#menu-btn');
 let header = document.querySelector('.header');
+let conf = document.querySelector('#menu-conf');
+let configuracion = document.querySelector('.configuracion')
 
 menu.onclick = ()=>{
     menu.classList.toggle('fa-times');
     header.classList.toggle('active')
+}
+conf.onclick = ()=>{
+    configuracion.classList.toggle('active');
 }
